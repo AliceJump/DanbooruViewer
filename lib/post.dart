0ad3ed9bc4ab7185a61bd91d468befed9a1ccc5a
AliceJump <149395013+AliceJump@users.noreply.github.com>
2026-07-28 18:51:12 +0800

    添加分享，跳转按钮添加长按复制功能

--- lib/post.dart
+++ lib/post.dart
@@ -11,6 +11,8 @@ class Post {
   final String? tagStringCopyright;
   final String? tagStringMeta;
   final String? source;
+  final int? imageWidth;
+  final int? imageHeight;
 
   Post({
     required this.id,
@@ -25,6 +27,8 @@ class Post {
     this.tagStringCopyright,
     this.tagStringMeta,
     this.source,
+    this.imageWidth,
+    this.imageHeight,
   });
 
   factory Post.fromJson(Map<String, dynamic> json) {
@@ -41,6 +45,8 @@ class Post {
       tagStringCopyright: json['tag_string_copyright'],
       tagStringMeta: json['tag_string_meta'],
       source: json['source'],
+      imageWidth: json['image_width'],
+      imageHeight: json['image_height'],
     );
   }
 }
